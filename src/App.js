bb551d55aff72c04080f99e8165d04e0c682eeba
diego <diego.me11911@gmail.com>
2021-09-16 01:22:12 -0500

    prueba 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import './App.css';
-import ímagen1 from './img/img002.jpg';
 
 function App() {
-  console.log("process: ", process);
-  console.log("P env: ", process.env);
-  console.log("P vers: ", process.versions);
-  console.log("P ver: ", process.version);
-  console.log("P title: ", process.title);
-  console.log("P browser: ", process.browser);
+  console.log("ENV: ", process.env);
+  console.log("REACT_APP_ENV: ", process.env.REACT_APP_ENV);
+  console.log("REACT_APP_API: ", process.env.REACT_APP_API);
   return (
     <div className="App">
-      CONSULTAS REACT
+      CONSULTAS REACT 2
       <div>
-        <img src={ímagen1} alt="imagen 1" width="200" height="200"/>
-        <br/>
-        {/* <img src={ímagen2} alt="imagen 2"/> */}
         <img src={`${process.env.PUBLIC_URL}/assets/img/img151.jpg`} alt="imagen 2"/>
       </div>
     </div>
